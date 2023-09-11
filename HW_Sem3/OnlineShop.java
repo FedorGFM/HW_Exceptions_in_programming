524e537d41619734ef0b70cd15038e63f8c8fb9f
Fedor <4ips228@gmail.com>
2023-09-11 22:55:13 +0700

    hw3

---
+++ HW_Sem3/OnlineShop.java
@@ -0,0 +1,70 @@
+package HW_Sem3;
+
+import java.util.Scanner;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(int productId) {
+        super("Продукт с ID " + productId + " не найден.");
+    }
+}
+
+class Product {
+    private String name;
+    private int availableQuantity;
+    private double price;
+
+    public Product(String name, int availableQuantity, double price) {
+        this.name = name;
+        this.availableQuantity = availableQuantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class ProductDatabase {
+    public static Product getProduct(int productId) throws ProductNotFoundException {
+        // Подразумевается обращение к базе данных или хранилищу товаров
+        // и получение информации о товаре по его ID
+        // В данном примере мы используем заглушку
+        if (productId == 1) {
+            return new Product("Product 1", 10, 5.99);
+        }
+        throw new ProductNotFoundException(productId);
+    }
+}
+
+class ShopManager {
+    public static double purchaseProduct(int productId, int quantity) throws ProductNotFoundException {
+        Product product = ProductDatabase.getProduct(productId);
+        double totalPrice = product.getPrice() * quantity;
+        return totalPrice;
+    }
+}
+
+public class OnlineShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter product ID: ");
+        int productId = scanner.nextInt();
+        System.out.print("Enter quantity: ");
+        int quantity = scanner.nextInt();
+
+        try {
+            double totalPrice = ShopManager.purchaseProduct(productId, quantity);
+            System.out.println("Total price: $" + totalPrice);
+        } catch (ProductNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
